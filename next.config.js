dcf47625975f0f823d1bf17036dc01f7f3c13ef9
SHAHRIAR AHMED <shahriar7ahmed@gmail.com>
2023-03-28 10:30:05 +0600

    Deploy

--- next.config.js
+++ next.config.js
@@ -19,7 +19,6 @@ const nextConfig = {
   ) {
     return {
       "/": { page: "/" },
-      "/about": { page: "/about" },
       "/p/hello-nextjs": { page: "/post", query: { title: "hello-nextjs" } },
       "/p/learn-nextjs": { page: "/post", query: { title: "learn-nextjs" } },
       "/p/deploy-nextjs": { page: "/post", query: { title: "deploy-nextjs" } },
